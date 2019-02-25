193c9ccd49794fd79440739a57a13e8a378d8ba6
Shehzad Muhammad <shehzad.muhammad@outlook.com>
2019-02-25 14:14:16 -0500

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 
 import AppNavBar from './components/AppNavBar';
 import MensItemsSection from './components/MensItemsSection';
+import ShoppingCartSection from './components/ShoppingCartSection';
+import WomensItemsSection from './components/WomensItemsSection';
+import ShoesItemsSection from './components/ShoesItemsSection';
 
 import { Provider } from 'react-redux';
 import store from './store';
+
+import { Route } from 'react-router-dom';
 
 
 import './App.css';
@@ -15,7 +20,11 @@ class App extends Component {
     <Provider store={store}>
       <div className="App">
         <AppNavBar />
-        <MensItemsSection />
+        <Route path="/Mens" component={MensItemsSection} />
+        <Route path="/Womens" component={WomensItemsSection} />
+        <Route path="/Shoes" component={ShoesItemsSection} />
+        <Route path="/Shopping Cart" component={ShoppingCartSection} />
+
       </div>
       </Provider>
     );
